4a377fdab948779d1e6606f5984f29c5e03a3280
JulioKoepsel <julio.koepsel@gmail.com>
2023-07-07 21:15:35 -0300

    Commit Final

--- testdb.sh
+++ testdb.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 docker exec mongo1 scripts/criar.sh
-echo ""
 docker exec mongo1 scripts/gravar.sh
-echo ""
 docker exec mongo1 scripts/listar.sh
-echo ""
 docker exec mongo1 scripts/editar.sh
-echo ""
 docker exec mongo1 scripts/listar.sh
-echo ""
 docker exec mongo1 scripts/remover.sh
-echo ""
 docker exec mongo1 scripts/listar.sh
-echo ""
